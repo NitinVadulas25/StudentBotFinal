8acb6ed3353797471e6de8ce6d9f27533bf9b73e
DhruvBharadwaj25 <dhruvbharadwaj25@mittymonarch.com>
2023-08-29 17:47:48 -0700

    some deletes

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -30,84 +30,18 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotInit() {
-        LedSubsystem.getInstance().initHardware();
-        Gyro.getInstance().initHardware();
-        SwerveSubsystem.getInstance().initHardware();
 
-        TelescopeSubsystem.getInstance().initHardware();
-        PivotSubsystem.getInstance().initHardware();
-        IntakeSubsystem.getInstance().initHardware();
-
-        PivotSubsystem.getInstance().setBrakeMode();
-        TelescopeSubsystem.getInstance().setBrakeMode();
-
-        Odometry.getInstance().FIELD_LEFT_SIDE = false;
-        double x = 0;
-        double y = 0;
-        double t = 0;
-        Gyro.getInstance().setAngleOffset(t);
-        Odometry.getInstance().setState(x, y, t);
-        SwerveSubsystem.getInstance().setPose(new Pose(new Point(0, 0),
-                new Angle(Gyro.getInstance().getHeadingRadians())));
-        Odometry.getInstance().setScoringCam(true);
     }
 
     @Override
     public void robotPeriodic() {
-        SwerveSubsystem.getInstance().updateForwardKinematics();
-
-        //UPDATE FROM BEAGLE AND JETSON
-        Odometry.getInstance().update();
-        ArmKinematics.updateAngleToGamePiece(StateMachine.getInstance().getCurrentPieceState()
-                == StateMachine.PieceState.CONE, 0);
-
-        LoggerInterface.getInstance().put("Heading", Gyro.getInstance().getHeadingRadians());
-        LoggerInterface.getInstance().put("Pose", Odometry.getInstance().getState());
-
-        LoggerInterface.getInstance().put("Pose X", Odometry.getInstance().getPose()[0]);
-        LoggerInterface.getInstance().put("Pose Y", Odometry.getInstance().getPose()[1]);
-
-//        LoggerInterface.getInstance().put("Spark Current", IntakeSubsystem.getInstance().getCurrent());
-//        System.out.println("CURRENT: " + IntakeSubsystem.getInstance().getCurrent());
-
-        CommandScheduler.getInstance().run();
 
     }
 
     @Override
     public void autonomousInit() {
-        Odometry.getInstance().FIELD_LEFT_SIDE = LoggerInterface.getInstance().getValue("fieldside").equals("left");
 
-        auto = LoggerInterface.getInstance().getValue("auto");
-        low = LoggerInterface.getInstance().getValue("autoside").equals("L");
-        bal = LoggerInterface.getInstance().getValue("autobal").equals("T");
-
-        switch(auto) {
-            case "preload":
-                new PreloadAndBalanceAuto(Odometry.getInstance().FIELD_LEFT_SIDE).schedule();
-                break;
-            case "balance":
-                new PPOneAuto(low, Odometry.getInstance().FIELD_LEFT_SIDE, StateMachine.PieceState.CUBE, bal).schedule();
-                break;
-            case "pick":
-                new PPTwoAuto(low, Odometry.getInstance().FIELD_LEFT_SIDE).schedule();
-                break;
         }
-
-
-//        Odometry.getInstance().FIELD_LEFT_SIDE = false;
-//
-//        new PPTwoAuto(false, false).schedule();
-
-//        Pose init = Odometry.getInstance().getState();
-//        Pose end = new Pose(Point.add(init.getPosition(), new Point(150, 100)), init.getHeading());
-//        SwervePath path = new SwervePath(new QuinticHermiteSpline(init, end),
-//                15, 3, 3, 3, 0, 0, true);
-//        new PathFollowingCommand(path, init.getHeading().getRadians() + Math.PI, 5, 0.05,
-//                0.1, 0.8, 6, 0, 0.02).schedule();
-
-//        new AutoScoreCommand(8, 0, 3, 3, 3, 0, 0).schedule();
-    }
 
     /**
      * This function is called periodically during autonomous.
@@ -121,24 +55,6 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void teleopInit() {
-        SwerveSubsystem.getInstance().setRampRate(0.5);
-        OI.getInstance().setupControls();
-        Odometry.getInstance().disableCustomCam();
-        Odometry.getInstance().setScoringCam(false);
-        OI.getInstance().zeroAll();
-        StateMachine.getInstance().setIntakeOff();
-
-
-//        new InitAutoCommand(new Pose(new Point(40.45 + 32, 42.19 - 20.873), new Angle(Math.PI))).schedule();
-
-//        SwerveSubsystem.getInstance().setRampRate(0);
-//        new FastOvershootBalance(3, 0.7, false).schedule();
-//        new Balance(false).schedule();
-        //for false
-//        new TimedBangBangBalance(2.5, 650, 0.3, false).schedule();
-        //for true
-//        new TimedBangBangBalance(2, 700, 0.5, true).schedule();
-
 
     }
 
@@ -147,9 +63,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void teleopPeriodic() {
-//        LoggerInterface.getInstance().put("AGNEL TO GP", ArmKinematics.getSplineToGamePiece());
-//    System.out.println("ANGLE: " + PivotSubsystem.getInstance().getPositionRadians());
-//    System.out.println("RADIUS: "  + TelescopeSubsystem.getInstance().getDistanceMeters());
+
     }
 
     /**
@@ -157,12 +71,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void disabledInit() {
-        TelescopeSubsystem.getInstance().setBrakeMode();
-        PivotSubsystem.getInstance().setBrakeMode();
 
-        LedSubsystem.getInstance().turnOff();
-
-//        SwerveSubsystem.getInstance().setAnglesZero();
     }
 
     /**
@@ -184,9 +93,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void testPeriodic() {
-//        OI.getInstance().getDriveController().setRumble(GenericHID.RumbleType.kLeftRumble, 0.5);
-//        OI.getInstance().getDriveController().setRumble(GenericHID.RumbleType.kRightRumble, 0.5);
-//        System.out.println(LoggerInterface.getInstance().getValue("fieldside"));
+
     }
 
     /**
@@ -194,6 +101,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void simulationInit() {
+
     }
 
     /**
@@ -201,5 +109,6 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void simulationPeriodic() {
+
     }
 }
