4c814212dc8a728fe648f17ff675ad105b67da53
NitinVadulas25 <91292971+NitinVadulas25@users.noreply.github.com>
2023-08-31 16:58:48 -0700

    push final todya

---
+++ src/main/java/com/github/mittyrobotics/drivetrain/commands/SwerveDefaultCommand.java
@@ -0,0 +1,62 @@
+package com.github.mittyrobotics.drivetrain.commands;
+
+import com.github.mittyrobotics.drivetrain.SwerveConstants;
+import com.github.mittyrobotics.drivetrain.SwerveSubsystem;
+import com.github.mittyrobotics.util.Gyro;
+import com.github.mittyrobotics.util.OI;
+import com.github.mittyrobotics.util.math.Angle;
+import com.github.mittyrobotics.util.math.Vector;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SwerveDefaultCommand extends CommandBase {
+    private double throttleX, throttleY, throttleAngular, joystickDeadzone;
+    public SwerveDefaultCommand() {
+        setName("Swerve Default Command");
+        addRequirements(SwerveSubsystem.getInstance());
+    }
+
+    @Override
+    public void initialize() {
+        throttleX = 0;
+        throttleY = 0;
+        throttleAngular = 0;
+        joystickDeadzone = 0;
+    }
+
+    @Override
+    public void execute() {
+        throttleX = -OI.getInstance().getDriverController().getLeftX();
+        throttleY = -OI.getInstance().getDriverController().getLeftY();
+        throttleAngular = -OI.getInstance().getDriverController().getRightX();
+
+
+        SwerveSubsystem.getInstance().calculateInputs(
+                new Vector(
+                        SwerveConstants.MAX_LINEAR_SPEED_INCHES_PER_SECOND * Math.abs(throttleX) * throttleX,
+                        SwerveConstants.MAX_LINEAR_SPEED_INCHES_PER_SECOND * Math.abs(throttleY) * throttleY
+                ),
+                SwerveConstants.MAX_ANGULAR_SPEED * throttleAngular
+        );
+
+
+        SwerveSubsystem.getInstance().applyCalculatedInputs();
+
+        SmartDashboard.putNumber("Desired Angle", SwerveSubsystem.getInstance().wheelSpeedAngle(3, 3, 3, 3)[1]);
+
+
+
+
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
